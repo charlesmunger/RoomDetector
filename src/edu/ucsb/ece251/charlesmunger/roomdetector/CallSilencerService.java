efbf701459d4a7d3969c4e94f814704b05b2d369
Charles Munger <charleslmunger@gmail.com>
2013-02-05 00:50:38 -0800

    Handle ending calls properly.

--- src/edu/ucsb/ece251/charlesmunger/roomdetector/CallSilencerService.java
+++ src/edu/ucsb/ece251/charlesmunger/roomdetector/CallSilencerService.java
@@ -5,21 +5,41 @@ import com.google.inject.Inject;
 import roboguice.service.RoboIntentService;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class CallSilencerService extends RoboIntentService {
 
 	private static final String TAG = "CallSilencerService";
 	@Inject AudioManager am;
+	@Inject TelephonyManager tm;
+	
 	public CallSilencerService() {
 		super(TAG);
 	}
 
 	@Override
 	public void onHandleIntent(Intent intent) {
-		Log.d(TAG,"Silencing ringer");
-		am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-		am.setStreamMute(AudioManager.STREAM_RING, true);
+		switch(tm.getCallState()) {
+		case TelephonyManager.CALL_STATE_IDLE: idle(); break;
+		case TelephonyManager.CALL_STATE_RINGING: ringing(); break;
+		case TelephonyManager.CALL_STATE_OFFHOOK: offHook(); break;
+		default: Log.wtf(TAG, "Unexpected state");
+		}
 	}
 
+	private void offHook() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void idle() {
+		Log.d(TAG, "Restoring ringer state");
+		am.setStreamMute(AudioManager.STREAM_RING,false);
+	}
+
+	private void ringing() {
+		Log.d(TAG, "Muting ringer stream");
+		am.setStreamMute(AudioManager.STREAM_RING, true);
+	}
 }
